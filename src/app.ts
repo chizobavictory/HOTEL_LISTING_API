98d4ebfbea0b9e2d816f038735045ce248a9c3b3
Chizoba Victory <chizoba.victory1@gmail.com>
2022-12-18 02:33:49 +0100

    Adding finfishing touches ✨

--- src/app.ts
+++ src/app.ts
@@ -13,27 +13,27 @@ mongoose.connect(process.env.MONGO_URL!, () => {
   console.log("Connected to MongoDB Database");
 });
 
+import indexRouter from "./routes/index";
 import hotelRouter from "./routes/hotel";
 import usersRouter from "./routes/users";
 
 const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "..", "views"));
-app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, "public")));
 
-// app.get('/', (req, res) => {
-//   res.sendFile('index.html', {root: path.join(__dirname, 'public')});
-// })
-
+app.use("/", indexRouter);
 app.use("/hotels", hotelRouter);
 app.use("/users", usersRouter);
+
+// view engine setup
+app.set('views', path.join(`${__dirname}/../`, 'views'))
+app.set("view engine", "ejs");
 
 // catch 404 and forward to error handler
 app.use(function (req: Request, res: Response, next: NextFunction) {
