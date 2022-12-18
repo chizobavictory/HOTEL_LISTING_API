22714f8d00b2ec17b1478639bbec044eb3f04818
Chizoba Victory <chizoba.victory1@gmail.com>
2022-12-18 01:04:21 +0100

    added hotel and user routes 🚏

---
+++ src/routes/users.ts
@@ -0,0 +1,37 @@
+import express from "express";
+
+import {
+  loginUser,
+  registerUsers,
+  getUsers,
+  renderRegisterPage,
+  renderLoginPage,
+  renderHomePage,
+  renderListingPage,
+  renderSecListingPage,
+  logout,
+  singleUser,
+  deleteUsers,
+  updateUsers,
+} from "../controller/userController";
+
+const router = express.Router();
+
+/* GET users listing. */
+router.post("/api/register", registerUsers);
+router.get("/api/allusers", getUsers);
+router.get("/api/find/:id", singleUser);
+router.delete("/api/:id", deleteUsers);
+router.put("/api/update/:id", updateUsers);
+
+router.post("/logins", loginUser);
+router.post("/logout", logout);
+
+// ejs routes
+router.get("/signup", renderRegisterPage);
+router.get("/signin", renderLoginPage);
+router.get("/home", renderHomePage);
+router.get("/listing1", renderListingPage);
+router.get("/listing2", renderSecListingPage);
+
+export default router;
