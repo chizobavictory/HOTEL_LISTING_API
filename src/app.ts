f41e5a720cbf1a712649cf753cc42188e16ec5f3
Chizoba Victory <chizoba.victory1@gmail.com>
2022-12-23 09:00:01 +0100

    Adding finfishing touches ✨

--- src/app.ts
+++ src/app.ts
@@ -32,8 +32,8 @@ app.use("/hotels", hotelRouter);
 app.use("/users", usersRouter);
 
 // view engine setup
-// app.set('views', path.join(`${__dirname}/../`, 'views'))
-// app.set("view engine", "ejs");
+app.set('views', path.join(`${__dirname}/../`, 'views'))
+app.set("view engine", "ejs");
 
 // catch 404 and forward to error handler
 app.use(function (req: Request, res: Response, next: NextFunction) {
